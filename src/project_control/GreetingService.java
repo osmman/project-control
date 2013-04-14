9f10ff6822b698163d3740a8680a8990babba3b6
Tomáš Turek <osmman@gmail.com>
2013-04-14 20:45:24 +0200

    Inicializace projektu s Jersey knihovnou

---
+++ src/project_control/GreetingService.java
@@ -0,0 +1,15 @@
+package project_control;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+ 
+@Path("/api/info/") /* Maps the service on url localhost:8888/api/info/ */
+public class GreetingService {
+ 
+    @GET /* Response on the GET request */
+    @Produces("text/plain") /* The result of response is plain text */
+    public String helloWorld() {
+    	return "Hello world!!!";
+    }
+}
